b183a39f453e9bc05570cb70e52d95921f10c7f0
immora <kasia_pi1@wp.pl>
2019-08-26 21:49:49 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ const sections = [
             <tr>
               <td>2010 - 2014</td>
               <td>
-                Apprentship Informatics, Federal Diploma of Vocational Education
+                Apprenticeship Informatics, Federal Diploma of Vocational Education
                 and Training
               </td>
             </tr>
@@ -60,7 +60,7 @@ const sections = [
             </tr>
             <tr>
               <td>English</td>
-              <td>Good knowlege and professional experience (~ C1)</td>
+              <td>Very good knowlege and professional experience (~ C1)</td>
             </tr>
           </tbody>
         </table>
@@ -101,7 +101,7 @@ const sections = [
               <td>Good knowledge</td>
             </tr>
             <tr>
-              <td>Networking</td>
+              <td>Computer networks</td>
               <td>Good knowlege</td>
             </tr>
             <tr>
@@ -118,12 +118,13 @@ const sections = [
         <h3>Personal skills</h3>
         <ul>
           <li>Fast learning skills and high intake capacity</li>
-          <li>Calm and endurant, especially under stress situation</li>
+          <li>Calm and endurant, especially under stress</li>
           <li>
-            Ability to work independant and in a team, including taking over
-            leadership roles in a team
+            Ability to work independently and in a team, including taking over
+            leadership roles
           </li>
           <li>Very short & precise communication style</li>
+          <li>todo: add more</li>
         </ul>
       </>
     ),
@@ -133,10 +134,10 @@ const sections = [
     title: "Interests & involvements",
     id: "interests",
     content: (
-      <>
+      <> todo: add interests
         <ul>
           <li>
-            Member of the organisation comitee of the running race "La Tzampata"
+            Member of organisation comitee of the running race "La Tzampata"
           </li>
           <li>Member of the comitee of the orienteering club "OLG Murten"</li>
           <li>Coach of the regional squad in orienteering "Cadre Romand"</li>
@@ -151,8 +152,8 @@ const IndexPage = () => (
     <SEO title="CV" />
     <h1>Yannis Güdel, Developer</h1>
     <p>
-      As a developer I always aim to make peoples life easier. After having
-      spent most of my work experience caring about the backend of websties, I
+      As a developer I always aim to make people's lifes easier. After having
+      spent most of my work experience caring about the backend of websites, I
       now look forward to make the next generation of web UIs enjoyable to use,
       fast and accessible.
     </p>
