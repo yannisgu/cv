8e7175440a703a520a8b16c876fb24317281581e
Yannis Güdel <me@yannisguedel.ch>
2019-08-29 13:38:52 +0200

    styled workplaces

--- src/components/workplace.js
+++ src/components/workplace.js
@@ -1,15 +1,26 @@
 import React from "react"
+import "./workplace.scss"
 
 const Workplace = ({ timeframe, title, company, children }) => {
   return (
-    <div class="workplace">
-      <h3>
-        <span class="workplace_title">{title}</span>
-        <br />
-        <span class="workplace_company">{company}</span>
-      </h3>
-      <div class="workplace__timeframe">{timeframe}</div>
-      {children}
+    <div className="workplace">
+      <div className="workplace__illustration">
+        <svg viewBox="0 0 100 100" className="workplace__circle">
+          <circle cx="50" cy="50" r="40" />
+        </svg>
+        <div className="workplace__line"></div>
+      </div>
+      <div className="workplace__content">
+        <div className="workplace__timeframe">{timeframe}</div>
+        <div className="workplace__description">
+          <h3 className="workplace__title">
+            <span className="workplace_title">{title}</span>
+            <br />
+            <span className="workplace_company">{company}</span>
+          </h3>
+          {children}
+        </div>
+      </div>
     </div>
   )
 }
